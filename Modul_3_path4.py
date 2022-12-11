dbae0f257c00a3364436b7e07d49d3199dc19c4d
Nakushnov Anatoliy <nakushnov@rambler.ru>
2022-12-11 21:59:10 +0400

    Часть 4 задание 1

--- Modul_3_path4.py
+++ Modul_3_path4.py
@@ -1,29 +1,39 @@
 print('Задание 1: ')
 import json
 x = {}
-q1 = input('Вход или Регистрация')
-if q1 == 'вход':
-    ...
-elif q1 == 'регистрация':
-    login = input('Введите логин: ')
-    password = input('Введите пароль: ')
-    if login not in x:
-        register(login, password)
 
 
 def register(login, password):
     with open('register.json', 'r', encoding='utf-8') as file:
         x = json.load(file)
+        login = input('Введите логин: ')
+        password = input('Введите пароль: ')
         if login not in x.keys():
             x[login] = password
             with open('register.json', 'w', encoding='utf-8') as file:
                 json.dump(login, file)
-
-
         else: print('Логин занят')
 
-
-
-
-
 def input_log():
+    with open('register.json', 'r', encoding='utf-8') as file:
+        x = json.load(file)
+        login = input('Введите логин: ')
+        password = input('Введите пароль: ')
+        if login in x:
+            if password == x[login]:
+                print('Вход разрешен!')
+            else:
+                print('Неверный пароль')
+        else:
+            print('Логин не найден')
+
+
+while True:
+    q1 = input('Вход или Регистрация')
+    if q1 == 'вход':
+        input_log()
+
+    elif q1 == 'регистрация':
+        register()
+    #else:
+        #break
